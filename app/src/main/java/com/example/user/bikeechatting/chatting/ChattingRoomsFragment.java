a9f893e81821396653fb85c331793597ebd4b483
ramblingnova <slless_des@naver.com>
2016-04-13 21:49:55 +0900

    7th

--- app/src/main/java/com/example/user/bikeechatting/chatting/ChattingRoomsFragment.java
+++ app/src/main/java/com/example/user/bikeechatting/chatting/ChattingRoomsFragment.java
@@ -16,10 +16,10 @@ import com.example.user.bikeechatting.R;
 import com.example.user.bikeechatting.chatting.room.ConversationActivity;
 import com.sendbird.android.MessagingChannelListQuery;
 import com.sendbird.android.SendBird;
+import com.sendbird.android.SendBirdNotificationHandler;
+import com.sendbird.android.model.Mention;
 import com.sendbird.android.model.MessagingChannel;
 
-import java.text.SimpleDateFormat;
-import java.util.Date;
 import java.util.List;
 
 import butterknife.Bind;
@@ -31,11 +31,14 @@ import butterknife.ButterKnife;
 public class ChattingRoomsFragment extends Fragment implements OnChattingRoomAdapterClickListener {
     @Bind(R.id.fragment_chatting_rooms_recycler_view)
     RecyclerView recyclerView;
+
+    private MessagingChannelListQuery mMessagingChannelListQuery;
     private ChattingRoomAdapter chattingRoomAdapter;
-    private static final String TAG = "CHATTING_ROOMS_FRAGMENT";
     private String appId;
     private String userId;
     private String userName;
+
+    private static final String TAG = "CHATTING_ROOMS_FRAGMENT";
 
     public static ChattingRoomsFragment newInstance(String appId, String userId, String userName, String gcmRegToken) {
         ChattingRoomsFragment chattingRoomsFragment = new ChattingRoomsFragment();
@@ -67,18 +70,58 @@ public class ChattingRoomsFragment extends Fragment implements OnChattingRoomAda
         userId = args.getString("USER_ID");
         userName = args.getString("USER_NAME");
 
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
+        final LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext(), LinearLayoutManager.VERTICAL, false);
         recyclerView.setLayoutManager(linearLayoutManager);
 
         chattingRoomAdapter = new ChattingRoomAdapter();
         chattingRoomAdapter.setOnChattingRoomAdapterClickListener(this);
-        init();
         recyclerView.setAdapter(chattingRoomAdapter);
 
         recyclerView.addOnScrollListener(new RecyclerView.OnScrollListener() {
             @Override
             public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
                 super.onScrollStateChanged(recyclerView, newState);
+
+                if ((linearLayoutManager.findLastVisibleItemPosition() == chattingRoomAdapter.getItemCount() - 1)
+                        && recyclerView.getChildCount() > 0) {
+                    if (mMessagingChannelListQuery != null
+                            && !mMessagingChannelListQuery.isLoading()
+                            && mMessagingChannelListQuery.hasNext()) {
+                        mMessagingChannelListQuery.next(new MessagingChannelListQuery.MessagingChannelListQueryResult() {
+                            @Override
+                            public void onResult(List<MessagingChannel> messagingChannels) {
+                                String reservationState = null;
+                                int i = 0;
+
+                                for (MessagingChannel messagingChannel : messagingChannels) {
+                                    switch (++i % 3) {
+                                        case 0:
+                                            reservationState = "RR";
+                                            break;
+                                        case 1:
+                                            reservationState = "RS";
+                                            break;
+                                        case 2:
+                                            reservationState = "RC";
+                                            break;
+                                    }
+
+                                    chattingRoomAdapter.add(
+                                            new ChattingRoomItem(
+                                                    messagingChannel,
+                                                    reservationState,
+                                                    "Bicycle Name" + i
+                                            )
+                                    );
+                                }
+                            }
+
+                            @Override
+                            public void onError(int i) {
+                            }
+                        });
+                    }
+                }
             }
 
             @Override
@@ -97,59 +140,35 @@ public class ChattingRoomsFragment extends Fragment implements OnChattingRoomAda
     }
 
     @Override
-    public void onChattingRoomAdapterClick(View view, ChattingRoomItem item, int position) {
-        Intent intent = new Intent(getActivity(), ConversationActivity.class);
-        intent.putExtra("messageChannelUrl", item.getMessageChannelUrl());
-        intent.putExtra("APP_ID", appId);
-        intent.putExtra("USER_ID", userId);
-        intent.putExtra("USER_NAME", userName);
-        intent.putExtra("userName", item.getUserName());
-        intent.putExtra("bicycleName", item.getBicycleName());
-        startActivity(intent);
-    }
+    public void onResume() {
+        super.onResume();
 
-    private void init() {
-        MessagingChannelListQuery mMessagingChannelListQuery = SendBird.queryMessagingChannelList();
-        mMessagingChannelListQuery.setLimit(30);
+        SendBird.registerNotificationHandler(new SendBirdNotificationHandler() {
+            @Override
+            public void onMessagingChannelUpdated(MessagingChannel messagingChannel) {
+                chattingRoomAdapter.replace(messagingChannel);
+            }
+
+            @Override
+            public void onMentionUpdated(Mention mention) {
+
+            }
+        });
+
+        if(mMessagingChannelListQuery == null) {
+            mMessagingChannelListQuery = SendBird.queryMessagingChannelList();
+            mMessagingChannelListQuery.setLimit(30);
+        }
+
         mMessagingChannelListQuery.next(new MessagingChannelListQuery.MessagingChannelListQueryResult() {
             @Override
             public void onResult(List<MessagingChannel> list) {
-                String messageChannelUrl;
-                String userImage = null;
+                chattingRoomAdapter.clear();
+
                 String reservationState = null;
-                String userName = null;
-                Date lastConversationTime;
-                String bicycleName = null;
-                String lastConversation;
-                int numOfStackedConversation;
                 int i = 0;
 
                 for (MessagingChannel messagingChannel : list) {
-                    for (MessagingChannel.Member member : messagingChannel.getMembers()) {
-                        if (!member.getId().equals(SendBird.getUserId())) {
-                            if (BuildConfig.DEBUG)
-                                Log.d(TAG, "member.getId : " + member.getId()
-                                                + ", member.getImageUrl : " + member.getImageUrl()
-                                                + ", member.getName : " + member.getName()
-                                );
-                            userImage = member.getImageUrl();
-                            userName = member.getName();
-                        }
-                    }
-                    SimpleDateFormat simpleDateFormat = new SimpleDateFormat("MM.dd HH:mm", java.util.Locale.getDefault());
-                    if (BuildConfig.DEBUG)
-                        Log.d(TAG, "messagingChannel.getId : " + messagingChannel.getId()
-                                        + ", messagingChannel.getUrl : " + messagingChannel.getUrl()
-                                        + ", messagingChannel.getLastMessage.getMessage : " + messagingChannel.getLastMessage().getMessage()
-                                        + ", messagingChannel.getLastMessageTimestamp : " + messagingChannel.getLastMessageTimestamp()
-                                        + ", messagingChannel.getUnreadMessageCount : " + messagingChannel.getUnreadMessageCount()
-                                        + ", refinedLastMessageTimestamp : " + simpleDateFormat.format(new Date(messagingChannel.getLastMessageTimestamp()))
-                        );
-                    messageChannelUrl = messagingChannel.getUrl();
-                    lastConversation = messagingChannel.getLastMessage().getMessage();
-                    lastConversationTime = new Date(messagingChannel.getLastMessageTimestamp());
-                    numOfStackedConversation = messagingChannel.getUnreadMessageCount();
-
                     switch (++i % 3) {
                         case 0:
                             reservationState = "RR";
@@ -164,18 +183,14 @@ public class ChattingRoomsFragment extends Fragment implements OnChattingRoomAda
 
                     chattingRoomAdapter.add(
                             new ChattingRoomItem(
-                                    messageChannelUrl,
-                                    userImage,
+                                    messagingChannel,
                                     reservationState,
-                                    userName,
-                                    lastConversationTime,
-                                    "Bicycle Name" + i,
-                                    lastConversation,
-                                    numOfStackedConversation
+                                    "Bicycle Name" + i
                             )
                     );
                 }
 
+                SendBird.join("");
                 SendBird.connect();
             }
 
@@ -184,33 +199,33 @@ public class ChattingRoomsFragment extends Fragment implements OnChattingRoomAda
 
             }
         });
+    }
 
-//        for (int i = 0; i < 30; i++) {
-//            String reservationState = null;
-//            switch (i % 3) {
-//                case 0:
-//                    reservationState = "RR";
-//                    break;
-//                case 1:
-//                    reservationState = "RS";
-//                    break;
-//                case 2:
-//                    reservationState = "RC";
-//                    break;
-//            }
-//            Date lastConversationTime = new Date();
-//            lastConversationTime.setTime(System.currentTimeMillis());
-//            chattingRoomAdapter.add(
-//                    new ChattingRoomItem(
-//                            "",
-//                            reservationState,
-//                            "User Name" + i,
-//                            lastConversationTime,
-//                            "Bicycle Name" + i,
-//                            "Last Conversation" + i,
-//                            i
-//                    )
-//            );
-//        }
+    @Override
+    public void onPause() {
+        super.onPause();
+        if(mMessagingChannelListQuery != null) {
+            mMessagingChannelListQuery.cancel();
+            mMessagingChannelListQuery = null;
+        }
+
+        SendBird.disconnect();
+    }
+
+    @Override
+    public void onChattingRoomAdapterClick(View view, ChattingRoomItem item, int position) {
+        Intent intent = new Intent(getActivity(), ConversationActivity.class);
+        intent.putExtra("messageChannelUrl", item.getMessagingChannel().getUrl());
+        intent.putExtra("APP_ID", appId);
+        intent.putExtra("USER_ID", userId);
+        intent.putExtra("USER_NAME", userName);
+        for (MessagingChannel.Member member : item.getMessagingChannel().getMembers())
+            if (!member.getId().equals(SendBird.getUserId())) {
+                intent.putExtra("TARGET_USER_NAME", member.getName());
+                break;
+            }
+        intent.putExtra("bicycleName", item.getBicycleName());
+        intent.putExtra("JOIN", true);
+        startActivity(intent);
     }
 }
